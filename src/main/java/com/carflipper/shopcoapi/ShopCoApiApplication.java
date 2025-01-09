f178b4fbbd17f5a677ff8c228f44a533e174f870
alexpoty <alexpotyagaylo@gmailc.om>
2025-01-09 19:32:58 +0200

    Initial Commit

---
+++ src/main/java/com/carflipper/shopcoapi/ShopCoApiApplication.java
@@ -0,0 +1,13 @@
+package com.carflipper.shopcoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopCoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopCoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carflipper/shopcoapi/ShopCoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carflipper.shopcoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopCoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
